71910dfe108eccbd27556c60b9344d34fd18afb9
gomta777 <gomta777@gmail.com>
2019-11-17 00:34:20 +0900

    game scnene change

--- gamescnene/gamescnene.py
+++ gamescnene/gamescnene.py
@@ -3,17 +3,11 @@ import pygame
 import sys
 import random
 
-
-
 def main():
     gamestate = 0  # 0 タイトル、1 ゲーム中、2 ゲームオーバー、-1 エラー
     pygame.init()  # Pygameを初期化
     screen = pygame.display.set_mode((900, 600))  # 画面を作成
     pygame.display.set_caption("Hit & Blow game")
-    # フォントの用意
-    # print(pygame.font.get_fonts())  # 使えるフォントの一覧を表示する
-    font1 = pygame.font.SysFont("yumincho", 20)
-    font2 = pygame.font.SysFont("meiryomeiryomeiryouimeiryouiitalic", 50)
 
     gamebutton = []
     gamebuttonrect = Rect(200, 250, 200, 50) # 画像の表示位置を表す矩形
@@ -21,14 +15,13 @@ def main():
     gamebutton.append(pygame.image.load("./images/judge.png"))
     gamebutton.append(pygame.image.load("./images/gameover.png"))
 
-
-
     running = True
     # メインループ
 
     while running:
         screen.fill((100, 100, 100))  # 背景色で塗る
-
+        # この時点では、　screen.blit(gamebutton[gamestate], gamebuttonrect) で済むけど、
+        # とりあえずは、if文でわけておく
         if gamestate == 0:
             screen.blit(gamebutton[0], gamebuttonrect)
         elif gamestate == 1:
@@ -37,7 +30,6 @@ def main():
             screen.blit(gamebutton[2], gamebuttonrect)
         else:
             print("error")
-
 
         for event in pygame.event.get():
             if event.type == QUIT:  # 終了イベント
@@ -56,6 +48,7 @@ def main():
                         gamestate = -1
 
         pygame.display.update()  # 描画処理を実行
+
 
 if __name__ == "__main__":
     main()
